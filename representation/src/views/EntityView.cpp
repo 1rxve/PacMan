5f0515f437b46d6fd4df6b028a132c8072bd142d
Odia <odiamhenhi@gmail.com>
2025-11-30 03:15:19 +0100

    - EntityView geimplementeerd, inherit van Observer

---
+++ representation/src/views/EntityView.cpp
@@ -0,0 +1,16 @@
+#include "representation/views/EntityView.h"
+
+namespace representation {
+    EntityView::EntityView(logic::EntityModel* model, sf::RenderWindow* window, const Camera* camera)
+            : model(model), window(window), camera(camera) {
+        model->attach(this);
+    }
+
+    EntityView::~EntityView() {
+        model->detach(this);
+    }
+
+    void EntityView::onNotify() {
+        draw();  // Roept subclass draw() aan (zonder parameter, gebruikt camera member)
+    }
+}
