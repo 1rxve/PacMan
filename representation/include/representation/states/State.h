ce86bd179357bd610130d32df661c2d4089631cb
Odia <odiamhenhi@gmail.com>
2025-12-12 20:02:57 +0100

    - State en StateManager aangemaakt

---
+++ representation/include/representation/states/State.h
@@ -0,0 +1,31 @@
+#ifndef PACMANGAME_STATE_H
+#define PACMANGAME_STATE_H
+
+#include <SFML/Graphics.hpp>
+#include "logic/patterns/AbstractFactory.h"  // ← CORRECT (same as EntityView doet)
+
+namespace representation {
+    class Camera;        // ← Forward declare (eigen namespace)
+    class StateManager;  // ← Forward declare (eigen namespace)
+
+    class State {
+    protected:
+        sf::RenderWindow* window;
+        logic::AbstractFactory* factory;
+        const Camera* camera;
+        StateManager* stateManager;
+
+    public:
+        State(sf::RenderWindow* win, logic::AbstractFactory* fac,
+              const Camera* cam, StateManager* sm)
+                : window(win), factory(fac), camera(cam), stateManager(sm) {}
+
+        virtual ~State() = default;
+
+        virtual void update(float deltaTime) = 0;
+        virtual void render() = 0;
+        virtual void handleEvent(const sf::Event& event) = 0;
+    };
+}
+
+#endif //PACMANGAME_STATE_H
